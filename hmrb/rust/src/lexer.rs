e5b8268cb744163d7bd9dd1afef60567ce34c6cb
Kristian Boda <kristian.boda@babylonhealth.com>
2022-09-05 12:04:56 +0100

    refactor: lexer

--- hmrb/rust/src/lexer.rs
+++ hmrb/rust/src/lexer.rs
@@ -1,15 +1,59 @@
 use pest::Parser;
+use pest::error::Error;
+use std::fs;
 
 #[derive(Parser)]
 #[grammar = "bab.pest"]
-pub struct BabParser;
+pub struct Grammar;
+
+enum Core<'a> {
+    Array(Vec<Core<'a>>),
+    String(&'a str),
+}
+
+fn lex(file: &str) -> Result<Core, Error<Rule>> {
+    fn parse_element(token: pest::iterators::Pair<Rule>) -> Core {
+        match token.as_rule() {
+            Rule::grammar => {
+                println!("GRAMMAR {}", token.as_str());
+                Core::Array(token.into_inner().map(parse_element).collect())}
+                ,
+            Rule::law => {
+                println!("LAW {}", token.as_str());
+                Core::Array(token.into_inner().map(parse_element).collect())
+            },
+            Rule::var => {
+                println!("LAW {}", token.as_str());
+                Core::Array(token.into_inner().map(parse_element).collect())
+            },
+            Rule::attributes => {
+                println!("ATTRS {}", token.as_str());
+                Core::Array(token.into_inner().map(parse_element).collect())
+            },
+            Rule::attribute => {
+                println!("ATTR {}", token.as_str());
+                Core::Array(token.into_inner().map(parse_element).collect())
+            },
+            Rule::name => {
+                println!("NAME {}", token.as_str());
+                Core::String(token.as_str())
+            },
+            Rule::value => {
+                println!("VALUE {}", token.as_str());
+                Core::String(token.as_str())
+            },
+            Rule::WHITESPACE => unreachable!()
+        }
+    }
+
+    let grammar_tree = Grammar::parse(Rule::grammar, &file)?.next().unwrap();
+    Ok(parse_element(grammar_tree))
+}
+
 
 #[test]
 fn test_lexer() {
-    let pairs = BabParser::parse(Rule::grammar, "Var test :").unwrap();
-    let tokens: Vec<_> = pairs.flatten().collect();
-    for token in tokens {
-        println!("{:?}", token);
-    }
+    let test_file = fs::read_to_string("tests/lexer.1").expect("cannot read file");
+    let core: Core = lex(&test_file).expect("unsuccessful parse");
     assert_eq!(true, true);
- }
+}
