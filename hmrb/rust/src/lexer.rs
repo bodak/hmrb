570b476eb98521a79eddbcbdfd06b48bf8e66432
Kristian Boda <kristian.boda@babylonhealth.com>
2022-09-08 08:45:08 +0100

    refactor: lexer

--- hmrb/rust/src/lexer.rs
+++ hmrb/rust/src/lexer.rs
@@ -1,7 +1,5 @@
-use pest::error::Error;
 use pest::Parser;
 use regex::Regex;
-use std::collections::HashMap;
 use std::fs;
 
 #[derive(Parser)]
@@ -9,40 +7,65 @@ use std::fs;
 pub struct Grammar;
 
 #[derive(Debug)]
-pub enum Core<'a> {
-    Array(Vec<Core<'a>>),
-    HashMap(&'a str, Regex),
+pub enum CoreElement {
+    Array(Vec<CoreElement>),
+    HashMap(String, Regex),
 }
 
-pub fn lexer(file: &str) -> Result<Core, Error<Rule>> {
-    fn parse_element(token: pest::iterators::Pair<Rule>) -> Core {
-        match token.as_rule() {
-            Rule::grammar => Core::Array(token.into_inner().map(parse_element).collect()),
-            Rule::law => Core::Array(token.into_inner().map(parse_element).collect()),
-            Rule::var => Core::Array(token.into_inner().map(parse_element).collect()),
-            Rule::attributes => Core::Array(token.into_inner().map(parse_element).collect()),
-            Rule::attribute => {
-                let mut tokens = token.into_inner();
-                let name = tokens.next().unwrap().as_str();
-                let value = tokens.next().unwrap().as_str();
-                Core::HashMap(name, Regex::new(&format!("{}", value)).unwrap())
+#[derive(Debug)]
+pub struct Core {
+    root: Vec<CoreElement>,
+    variable: bool,
+    name: String,
+}
+
+pub trait CoreFns {
+    fn new(file: &str) -> Self;
+}
+
+impl CoreFns for Core {
+    fn new(file: &str) -> Core {
+        fn parse_element(token: pest::iterators::Pair<Rule>) -> CoreElement {
+            match token.as_rule() {
+                Rule::law => CoreElement::Array(token.into_inner().map(parse_element).collect()),
+                Rule::var => CoreElement::Array(token.into_inner().map(parse_element).collect()),
+                Rule::attributes => {
+                    CoreElement::Array(token.into_inner().map(parse_element).collect())
+                }
+                Rule::attribute => {
+                    let mut tokens = token.into_inner();
+                    let name = tokens.next().unwrap().as_str();
+                    let value = tokens.next().unwrap().as_str();
+                    CoreElement::HashMap(
+                        String::from(name),
+                        Regex::new(&format!("{}", value)).unwrap(),
+                    )
+                }
+                Rule::grammar => unreachable!(),
+                Rule::name => unreachable!(),
+                Rule::value => unreachable!(),
+                Rule::WHITESPACE => unreachable!(),
             }
-            Rule::name => unreachable!(),
-            Rule::value => unreachable!(),
-            Rule::WHITESPACE => unreachable!(),
+        }
+        let grammar_tree = Grammar::parse(Rule::grammar, &file)
+            .unwrap()
+            .next()
+            .unwrap();
+        let root: Vec<CoreElement> = grammar_tree.into_inner().map(parse_element).collect();
+        Core {
+            root: root,
+            variable: false,
+            name: String::from("default"),
         }
     }
-
-    let grammar_tree = Grammar::parse(Rule::grammar, &file)?.next().unwrap();
-    Ok(parse_element(grammar_tree))
 }
 
 #[test]
 fn test_lexer_1() {
     let test_file = fs::read_to_string("tests/lexer.1").expect("cannot read file");
-    let core: Core = lexer(&test_file).expect("unsuccessful parse");
+    let core = Core::new(&test_file);
     assert_eq!(
         format!("{:?}", core),
-        "Array([Array([Array([HashMap(\"lemma\", Hello), HashMap(\"lemma\", World)])])])"
+        "Core { root: [Array([Array([HashMap(\"lemma\", Hello), HashMap(\"lemma\", World)])])], variable: false, name: \"default\" }"
     );
 }
